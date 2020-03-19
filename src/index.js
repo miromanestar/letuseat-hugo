f82e7c05925abbb0c149b7c9c39185353b81cc09
Miro Manestar <miroimanestar@msn.com>
2020-03-19 16:32:50 -0400

    Added fancybox for blog images

--- src/index.js
+++ src/index.js
@@ -9,4 +9,17 @@ import "./css/main.css";
       $('.navigation').removeClass('nav-bg');
         }
     });
+
+    //Function to add fancybox to images on blogs...
+    if(window.location.pathname.includes('blog')) {
+        $("img").each(function() {
+            var $this = $(this);
+            var src = $this.attr('src');
+            var caption = $this.attr('alt')
+            $this.addClass('image');
+            var a = $('<a/>').attr({'href': src, 'data-fancybox': 'images', 'data-caption': caption});
+            $this.wrap(a);
+        });
+    }
+
 })(jQuery);
