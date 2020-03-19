4500e397d81315f925d0ad3541a53d1e8cc9bdae
Miro Manestar <miroimanestar@msn.com>
2020-03-19 16:46:38 -0400

    Removed fancybox for logo

--- src/index.js
+++ src/index.js
@@ -13,12 +13,14 @@ import "./css/main.css";
     //Function to add fancybox to images on blogs...
     if(window.location.pathname.includes('blog')) {
         $("img").each(function() {
-            var $this = $(this);
-            var src = $this.attr('src');
-            var caption = $this.attr('alt')
-            $this.addClass('image');
-            var a = $('<a/>').attr({'href': src, 'data-fancybox': 'images', 'data-caption': caption});
-            $this.wrap(a);
+            if($(this).attr('alt') !=== 'Let Us Eat') {
+                var $this = $(this);
+                var src = $this.attr('src');
+                var caption = $this.attr('alt')
+                $this.addClass('image');
+                var a = $('<a/>').attr({'href': src, 'data-fancybox': 'images', 'data-caption': caption});
+                $this.wrap(a);
+            }
         });
     }
 
